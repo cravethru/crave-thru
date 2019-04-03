4103c9af69b5fb9b6dcbb925c22e58b14f5bdbbe
JoseCrescencio <jcrescencio@csumb.edu>
2019-04-03 14:23:37 -0700

    Intialize xcode project

---
+++ CraveThru/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CraveThru
+//
+//  Created by Angel on 4/3/19.
+//  Copyright © 2019 Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
