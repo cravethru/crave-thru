21c068d2a7efee5a6f608bf8574f9540dd100f70
resteybar <resteybar@csumb.edu>
2019-04-08 23:54:43 -0700

    In Progress: Able to see User location, but need to get the zoom in correct

--- craveThru/craveThru/MapsViewController.swift
+++ craveThru/craveThru/MapsViewController.swift
@@ -15,37 +15,35 @@ class MapsViewController: UIViewController {
     @IBOutlet weak var map_view: MKMapView!
     
     let location_manager = CLLocationManager()
+    let region_in_meters: Double = 10000
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("Starting Maps")
         
-        // 1. Latitude & Longitude of "Monterey"
-        let location = CLLocationCoordinate2D(latitude: 36.600256, longitude: -121.8946388)
-        
-        // 2. How far the view is
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let region = MKCoordinateRegion(center: location, span: span)
-        map_view.setRegion(region, animated: true)
-        
-        // 3. Create Annotation
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "Monterey"
-        annotation.subtitle = "California"
-        map_view.addAnnotation(annotation)
+        checkLocationServices()
     }
     
     func setupLocationManager() {
-        location_manager.delegate = self
+        location_manager.delegate = self as? CLLocationManagerDelegate
         location_manager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func centerViewOnUserLocation() {
+        if let location = location_manager.location?.coordinate {
+            
+            // Lat + Lon = How far we're zoomed in
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: region_in_meters, longitudinalMeters: region_in_meters)
+            map_view.setRegion(region, animated: true)
+        }
     }
     
     func checkLocationServices() {
         if CLLocationManager.locationServicesEnabled() {
             // Setup location manager
             setupLocationManager()
+            checkLocationAuthorization()
         } else {
             // Show alert letting the user know they have to turn this on
         }
@@ -53,28 +51,32 @@ class MapsViewController: UIViewController {
     
     func checkLocationAuthorization() {
         switch CLLocationManager.authorizationStatus() {
-        case .authorizedWhenInUse:
+        case .authorizedWhenInUse:  // When App Open, Get located when in use
+            // Do Map Stuff
+            map_view.showsUserLocation = true // Puts blue dot on map
             break
-        case .denied:
+        case .denied:               // Not allowed, denied once? Pop up won't show up
+            // Show alert instructing them how to turn on permission
             break
         case .notDetermined:
+            location_manager.requestWhenInUseAuthorization()
+        case.restricted:            // User cannot change app status, Ex: Parent can restrict child's location
+            // Show an alert letting them know
             break
-        case.restricted:
-            break
-        case.authorizedAlways:
+        case.authorizedAlways:      // Location is always on when app not and in use
             break
         }
     }
 }
-
-extension MapsViewController: CLLocationManagerDelegate {
-    // Do something when location updates
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        <#code#>
-    }
-    
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        <#code#>
-    }
-}
+//
+//extension MapsViewController: CLLocationManagerDelegate {
+//    // Do something when location updates
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        <#code#>
+//    }
+//    
+//    
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//        <#code#>
+//    }
+//}
