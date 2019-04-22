c817ce2fc298ae1079b45b34fd47ac678fae4975
resteybar <resteybar@csumb.edu>
2019-04-22 13:31:41 -0700

    Added pins to all restaurants around the area

--- craveThru/craveThru/MapsViewController.swift
+++ craveThru/craveThru/MapsViewController.swift
@@ -25,12 +25,52 @@ class MapsViewController: UIViewController {
     let client_secret = "OQ0F5RZWB53GZSWIKC4YWBTTJ4IDXQPPICLZQEUD3GQITVAA"
     let food_id = "4d4b7105d754a06374d81259"
     
+    // All Restaurants around User Location
+    var restaurants = [Restaurant]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("Starting Maps")
         
         checkLocationServices()
+        
+        // Example
+        // To display annotations
+        // Annotation View:
+        //  - Title
+        //  - Subtitle -> location_name
+//        let sample_starbucks = Restaurant(title: "Starbucks Imagination", location_name: "Imagination Street", coordinate: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.431297))
+//        map_view.addAnnotation(sample_starbucks)
+        
+        map_view.delegate = self
+    }
+    
+    func fetchData(location: CLLocation) {
+        let search_url = "https://api.foursquare.com/v2/venues/search?ll=\(location.coordinate.latitude),\(location.coordinate.longitude)&categoryId=\(food_id)&client_id=\(client_id)&client_secret=\(client_secret)&v=20190421"
+        
+        guard let search_request = URL(string: search_url) else { return }
+        
+        // Parses JSON from "Search for Venues" request
+        URLSession.shared.dataTask(with: search_request) { (data, response, err) in
+            guard let data = data else {return}
+            
+            // - Read JSON -> Store in Dictionary
+            if let json = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                if let unwrapped_response = json["response"] as? [String: Any] {
+                    let venues_json: [[String: Any]] = unwrapped_response["venues"] as! [[String: Any]]
+                    
+                    for venue_json in venues_json {
+                        if let venue = Restaurant.from(restaurant: venue_json) {
+                            self.restaurants.append(venue)
+                        }
+                    }
+                }
+            }
+            }.resume()
+        
+        //        let restaurant_id = "428a8580f964a52083231fe3"
+        //        let menu_url = "https://api.foursquare.com/v2/venues/\(restaurant_id)/menu?client_id=\(client_id)&client_secret=\(client_secret)&v=20190421"
     }
     
     func setupLocationManager() {
@@ -85,20 +125,14 @@ extension MapsViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return } // if nil, do nothing
         
-        let jsonString = "https://api.foursquare.com/v2/venues/search?ll=\(location.coordinate.latitude),\(location.coordinate.longitude)&categoryId=\(food_id)&client_id=\(client_id)&client_secret=\(client_secret)&v=20190417"
-        
-        guard let url = URL(string: jsonString) else { return }
-        
-        URLSession.shared.dataTask(with: url){ (data, response, err) in
-            guard let data = data else {return}
-            let dataAsString = String(data: data, encoding: .utf8)
-            print("hello")
-            print(dataAsString!)
-        }.resume()
-        
         // Makes the zoom in stay the position as the user moves
         let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
         let region = MKCoordinateRegion.init(center: center, latitudinalMeters: region_in_meters, longitudinalMeters: region_in_meters)
+        
+        // Makes "Search" request from Places API
+        fetchData(location: location)
+        map_view.addAnnotations(restaurants)
+        print("-----Restaurant count: \(restaurants.count)-----")
         
         map_view.setRegion(region, animated: true)
     }
@@ -108,5 +142,32 @@ extension MapsViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         // When authorization changes
         checkLocationAuthorization()
+    }
+}
+
+extension MapsViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? Restaurant {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            
+            // Pinning
+            //  - if   -> Already Created the view? Reuse Annotation View
+            //  - else -> Create a new Annotation View
+            if let dequeued_view = map_view.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView {
+                dequeued_view.annotation = annotation
+                view = dequeued_view
+            } else {
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true  // Decides to show (or not) the window
+                view.animatesDrop = true    // Shows pin dropping from the top of screen
+                view.calloutOffset = CGPoint(x: -5, y: 5)   // Where to place bubble
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView    // i button -> More info
+            }
+            
+            return view
+        }
+        
+        return nil
     }
 }
---
+++ craveThru/craveThru/Restaurant.swift
@@ -0,0 +1,51 @@
+//
+//  Restaurant.swift
+//  craveThru
+//
+//  Created by Raymond Esteybar on 4/22/19.
+//  Copyright © 2019 Eros Gonzalez. All rights reserved.
+//
+
+import MapKit
+import AddressBook
+
+class Restaurant: NSObject, MKAnnotation {
+    let title: String?
+    let location_name: String?
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, location_name: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title;
+        self.location_name = location_name
+        self.coordinate = coordinate
+    }
+    
+    var subtitle: String? {
+        return location_name
+    }
+    
+    class func from(restaurant: [String: Any]) -> Restaurant? {
+        var title: String = ""
+        var address: String = ""
+        var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+        
+            
+        // - Read JSON -> Store in Dictionary
+        //  - Get Restaurant Name
+        if let unwrapped_title = restaurant["name"] as? String {
+            title = unwrapped_title
+        } else {
+            title = ""
+        }
+        
+        //  - Get Location
+        if let unwrapped_location = restaurant["location"] as? [String: Any] {
+            address = unwrapped_location["address"] as? String ?? ""
+            let lat = unwrapped_location["lat"] as? Double ?? 0
+            let lng = unwrapped_location["lng"] as? Double ?? 0
+            coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+        }
+    
+        return Restaurant(title: title, location_name: address, coordinate: coordinate)
+    }
+}
