262a5ac68b3f03ffeb82e1331498fae8b71df5dd
erosgonzalez <egonzalez-lopez@csumb.edu>
2019-05-05 21:15:38 -0700

    Detail view setup

--- craveThru/craveThru/ExpandedViewController.swift
+++ craveThru/craveThru/ExpandedViewController.swift
@@ -9,29 +9,59 @@
 
 import UIKit
 
-class ExpandedViewController: UIViewController {
+class ExpandedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var imageSwipe: UIImageView!
     
-    
-    @IBOutlet weak var rexName: UILabel!
-    
-    @IBOutlet weak var rexPrice: UILabel!
-    
-    
-    @IBOutlet weak var rexDescription: UILabel!
-    
-    
-    @IBOutlet weak var rexPicture: UIImageView!
-    
-    
+    let imageNames = ["1", "2", "3", "4", "5"]
+    var currentImage = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGestureRight))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        imageSwipe.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGestureLeft))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        imageSwipe.addGestureRecognizer(swipeLeft)
+        
+        //TODO LEFT RIGHT TAPS
+        
         // Do any additional setup after loading the view.
     }
     
     
+    
+    @objc func respondToSwipeGestureRight() {
+        if currentImage == 0 {
+            currentImage = imageNames.count - 1
+        }else{
+            currentImage -= 1
+        }
+        imageSwipe.image = UIImage(named: imageNames[currentImage])
+    }
+    
+    @objc func respondToSwipeGestureLeft() {
+        if currentImage == imageNames.count - 1 {
+            currentImage = 0
+            
+        }else{
+            currentImage += 1
+        }
+        imageSwipe.image = UIImage(named: imageNames[currentImage])
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "MenuTableViewCell", for: indexPath) as! MenuTableViewCell
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
